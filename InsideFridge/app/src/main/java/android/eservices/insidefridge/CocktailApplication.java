145dc2f52babd100bfd3b9b59b40e7f1e913d342
Massiaux Alexis <alexis.massiaux@worldline.com>
2020-01-12 12:01:59 +0100

    [FEAT] CocktailApplication

---
+++ InsideFridge/app/src/main/java/android/eservices/insidefridge/CocktailApplication.java
@@ -0,0 +1,13 @@
+package android.eservices.insidefridge;
+
+import android.app.Application;
+
+public class CocktailApplication extends Application {
+
+    public static final String API_KEY = "307a2dfe3cmshab5301ebabb5427p126057jsn155145a71bdb";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
