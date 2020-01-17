40ec45610500c70d8d6acb351e9f2707cdabade8
Massiaux Alexis <alexis.massiaux@worldline.com>
2020-01-18 00:23:50 +0100

    [FEAT] CocktailRemoteDataSource

---
+++ InsideFridge/app/src/main/java/android/eservices/insidefridge/data/repository/remote/CocktailDisplayRemoteDataSource.java
@@ -0,0 +1,20 @@
+package android.eservices.insidefridge.data.repository.remote;
+
+import android.eservices.insidefridge.CocktailApplication;
+import android.eservices.insidefridge.data.api.CocktailDisplayService;
+import android.eservices.insidefridge.data.api.model.CocktailSearchResponse;
+
+import io.reactivex.Single;
+
+public class CocktailDisplayRemoteDataSource {
+
+        private CocktailDisplayService cocktailDisplayService;
+
+        public CocktailDisplayRemoteDataSource(CocktailDisplayService cocktailDisplayService) {
+            this.cocktailDisplayService = cocktailDisplayService;
+        }
+
+        public Single<CocktailSearchResponse> getCocktailSearchResponse(String keywords) {
+            return this.cocktailDisplayService.getCocktails(keywords, CocktailApplication.API_KEY);
+        }
+}
