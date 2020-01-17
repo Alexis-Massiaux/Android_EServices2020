65972df1bedbc2f4e2a079b91cfc8202c46a69d2
Massiaux Alexis <alexis.massiaux@worldline.com>
2020-01-18 00:21:30 +0100

    [FEAT] CocktailViewModelMapper

---
+++ InsideFridge/app/src/main/java/android/eservices/insidefridge/presentation/cocktaildisplay/search/mapper/CocktailToViewModelMapper.java
@@ -0,0 +1,29 @@
+package android.eservices.insidefridge.presentation.cocktaildisplay.search.mapper;
+
+import android.eservices.insidefridge.data.api.model.Cocktail;
+import android.eservices.insidefridge.presentation.cocktaildisplay.search.adapter.CocktailItemViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CocktailToViewModelMapper {
+
+    private CocktailItemViewModel map(Cocktail cocktail) {
+        CocktailItemViewModel cocktailItemViewModel = new CocktailItemViewModel();
+
+        cocktailItemViewModel.setId(cocktail.getId());
+        cocktailItemViewModel.setName(cocktail.getCocktailInfo().getName());
+
+        return cocktailItemViewModel;
+    }
+
+    public List<CocktailItemViewModel> map(List<Cocktail> cocktailList) {
+        List<CocktailItemViewModel> result = new ArrayList<>();
+
+        for(Cocktail cocktail : cocktailList)
+            result.add(map(cocktail));
+
+        return result;
+    }
+
+}
