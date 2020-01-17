8f16f692b5e851ba39d0ecb25a043b43a3527ede
Massiaux Alexis <alexis.massiaux@worldline.com>
2020-01-18 00:23:29 +0100

    [FEAT] Added CocktailInfo to CocktailObject

--- InsideFridge/app/src/main/java/android/eservices/insidefridge/data/api/model/Cocktail.java
+++ InsideFridge/app/src/main/java/android/eservices/insidefridge/data/api/model/Cocktail.java
@@ -3,9 +3,14 @@ package android.eservices.insidefridge.data.api.model;
 public class Cocktail {
 
     private String id;
+    private CocktailInfo cocktailInfo;
 
     public String getId() {
         return id;
+    }
+
+    public CocktailInfo getCocktailInfo() {
+        return cocktailInfo;
     }
 
 }
