0dfd893ebf2f1505314c7c2b3423e82ce7acd329
Massiaux Alexis <alexis.massiaux@worldline.com>
2020-01-18 11:46:30 +0100

    [FEAT] Set context to cocktailApplication

--- InsideFridge/app/src/main/java/android/eservices/insidefridge/CocktailApplication.java
+++ InsideFridge/app/src/main/java/android/eservices/insidefridge/CocktailApplication.java
@@ -2,6 +2,10 @@ package android.eservices.insidefridge;
 
 import android.app.Application;
 
+import android.eservices.insidefridge.data.di.FakeDependencyInjection;
+
+import com.facebook.stetho.Stetho;
+
 public class CocktailApplication extends Application {
 
     public static final String API_KEY = "307a2dfe3cmshab5301ebabb5427p126057jsn155145a71bdb";
@@ -9,5 +13,7 @@ public class CocktailApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Stetho.initializeWithDefaults(this);
+        FakeDependencyInjection.setContext(this);
     }
 }
