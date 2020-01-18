f29e8edb3049502c5293fb0712117ecc11575867
Massiaux Alexis <alexis.massiaux@worldline.com>
2020-01-18 11:44:29 +0100

    [FEAT] CocktailDisplayDataRepository to DI

--- InsideFridge/app/src/main/java/android/eservices/insidefridge/data/di/FakeDependencyInjection.java
+++ InsideFridge/app/src/main/java/android/eservices/insidefridge/data/di/FakeDependencyInjection.java
@@ -1,9 +1,16 @@
 package android.eservices.insidefridge.data.di;
 
+import android.content.Context;
 import android.eservices.insidefridge.data.api.CocktailDisplayService;
+import android.eservices.insidefridge.data.api.model.CocktailSearchResponse;
+import android.eservices.insidefridge.data.repository.CocktailDisplayDataRepository;
+import android.eservices.insidefridge.data.repository.CocktailDisplayRepository;
+import android.eservices.insidefridge.data.repository.remote.CocktailDisplayRemoteDataSource;
 
 import com.google.gson.Gson;
 import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import io.reactivex.Single;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -14,9 +21,16 @@ public class FakeDependencyInjection {
 
     private static Gson gson;
     private static Retrofit retrofit;
-
+    private static Context applicationContext;
     private static CocktailDisplayService cocktailDisplayService;
+    private static CocktailDisplayRepository cocktailDisplayRepository;
 
+
+    public static CocktailDisplayRepository getCocktailDisplayRepository() {
+        if(cocktailDisplayRepository == null)
+            cocktailDisplayRepository = new CocktailDisplayDataRepository(new CocktailDisplayRemoteDataSource(getCocktailDisplayService()));
+        return cocktailDisplayRepository;
+    }
     public static CocktailDisplayService getCocktailDisplayService() {
         if(cocktailDisplayService == null) {
             cocktailDisplayService = getRetrofit().create(CocktailDisplayService.class);
@@ -40,13 +54,17 @@ public class FakeDependencyInjection {
                     .addNetworkInterceptor(new StethoInterceptor())
                     .build();
             retrofit = new Retrofit.Builder()
-                    .baseUrl("https://the-cocktail-db.p.rapidapi.com/filter.php")
+                    .baseUrl("https://the-cocktail-db.p.rapidapi.com/")
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .client(client)
                     .addConverterFactory(GsonConverterFactory.create(getGson()))
                     .build();
         }
         return retrofit;
+    }
+
+    public static void setContext(Context context) {
+        applicationContext = context;
     }
 
 }
