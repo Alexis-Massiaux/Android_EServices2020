9601f2a37cf59942033452b17d2ad3f9eeb4e3ad
Massiaux Alexis <alexis.massiaux@worldline.com>
2020-01-19 16:51:53 +0100

    [FEAT] getCocktailFromId (api call)

--- InsideFridge/app/src/main/java/android/eservices/insidefridge/data/api/CocktailDisplayService.java
+++ InsideFridge/app/src/main/java/android/eservices/insidefridge/data/api/CocktailDisplayService.java
@@ -1,5 +1,6 @@
 package android.eservices.insidefridge.data.api;
 
+import android.eservices.insidefridge.data.api.model.Cocktail;
 import android.eservices.insidefridge.data.api.model.CocktailSearchResponse;
 
 import io.reactivex.Single;
@@ -12,4 +13,8 @@ public interface CocktailDisplayService {
     @Headers({"x-rapidapi-host: the-cocktail-db.p.rapidapi.com", "x-rapidapi-key: 307a2dfe3cmshab5301ebabb5427p126057jsn155145a71bdb"})
     @GET("filter.php")
     Single<CocktailSearchResponse> getCocktails(@Query("i") String keywords, @Query("key") String apiKey);
+
+    @Headers({"x-rapidapi-host: the-cocktail-db.p.rapidapi.com", "x-rapidapi-key: 307a2dfe3cmshab5301ebabb5427p126057jsn155145a71bdb"})
+    @GET("lookup.php")
+    Single<Cocktail> getCocktailFromId(@Query("i") String cocktailID);
 }
